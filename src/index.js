d1efe34559fae4d9c01a5cb5d93404372d308f91
ProDIGGY-vinilos <cristiangerster@gmail.com>
2022-10-08 17:31:09 -0300

    TG-08: Problems commitings

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 // use "import" to import libraries
 import express from 'express';
 
-// use "require" to import JSON files
-const admins = require('./data/admins.json');
-
 const app = express();
 const port = process.env.PORT || 3000;
+const employeeRouter = require('./resources/employees');
 
 app.use(express.json());
 
+app.use('/emp', employeeRouter);
+
 app.get('/', async (req, res) => {
   res.send('Hello World!');
-});
-
-app.get('/admins', (req, res) => {
-  res.status(200).json({
-    data: admins,
-  });
 });
 
 app.listen(port, () => {
--- src/resources/employees.js
+++ src/resources/employees.js
@@ -1,1 +1,31 @@
-// put y delete
+// put delete
+// import express from 'express';
+
+const express = require('express');
+
+const fs = require('fs');
+// import fs from 'fs';
+
+// use "require" to import JSON files
+const employees = require('../data/employees.json');
+
+const router = express.Router();
+
+router.get('/getAll', (req, res) => {
+  res.send(employees);
+});
+//
+router.delete('/delete/:id', (req, res) => {
+  const reqId = req.params.id;
+  const filtered = employees.filter((emp) => emp.id !== reqId);
+  // console.log(filtered);
+  fs.writeFile('../data/employees.json', JSON.stringify(filtered), (err) => {
+    if (err) {
+      res.send('Cannot delete user');
+    } else {
+      res.send('User deleted');
+    }
+  });
+});
+
+module.exports = router;
