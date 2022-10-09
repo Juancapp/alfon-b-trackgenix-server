7259fb545cd759ecedcda710cda07a0fa18307e0
sofihoj <sofiahojberg@gmail.com>
2022-10-09 15:57:53 -0300

    TG-W08: admins put method done

--- src/resources/admins.js
+++ src/resources/admins.js
@@ -10,7 +10,7 @@ router.delete('/delete/:id', (req, res) => {
   // console.log(filteredAdmin);
   fs.writeFile('src/data/admins.json', JSON.stringify(filteredAdmin), (err) => {
     if (err) {
-      res.send(`Cannot delete admin with id ${adminID}`);
+      res.send('Admin cannot be deleted');
     } else {
       res.send(`Admin with id ${adminId} deleted`);
     }
@@ -18,29 +18,29 @@ router.delete('/delete/:id', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-    const adminId = req.params.id;
-    const filteredAdmin = admins.find((admin) => admin.id !== adminId);
-    if (filteredAdmin) {
-        const updAdmin = req.body;
-        admins.forEach(admin => {
-            if (admin.id == parseInt(req.params.id)) {
-                admin.first_name = updAdmin.first_name ? updAdmin.first_name : admin.first_name;
-                admin.last_name = updAdmin.last_name ? updAdmin.last_name : admin.last_name;
-                admin.email = updAdmin.email ? updAdmin.email : admin.email;
-                admin.password = updAdmin.password ? updAdmin.password : admin.password;
-                fs.writeFile('src/data/admins.json', JSON.stringify(admins), (err) => {
-                    if (err) {
-                        res.send('Problem when adding admin');
-                    } else {
-                        res.send('Admin created');
-                    }
-                });
-                res.json({ msg: 'Admin updated', admin });
-            }
+  const adminId = req.params.id;
+  const filteredAdmin = admins.find((admin) => admin.id !== adminId);
+  if (filteredAdmin) {
+    const updAdmin = req.body;
+    admins.forEach((admin) => {
+      if (admin.id === parseInt(req.params.id, 10)) {
+        admin.first_name = updAdmin.first_name ? updAdmin.first_name : admin.first_name; // eslint-disable-line
+        admin.last_name = updAdmin.last_name ? updAdmin.last_name : admin.last_name; // eslint-disable-line
+        admin.email = updAdmin.email ? updAdmin.email : admin.email; // eslint-disable-line
+        admin.password = updAdmin.password ? updAdmin.password : admin.password; // eslint-disable-line
+        fs.writeFile('src/data/admins.json', JSON.stringify(admins), (err) => {
+          if (err) {
+            res.send('Problem when adding admin');
+          } else {
+            res.send('Admin created');
+          }
         });
-    } else {
-        res.send(`No admin with the id ${adminId} found`);
-    }
+        res.json({ msg: 'Admin updated', admin });
+      }
+    });
+  } else {
+    res.send(`No admin with the id ${adminId} found`);
+  }
 });
 
 module.exports = router;
