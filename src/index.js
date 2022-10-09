46a356fc78f8ffa21cabb6c902b55dd8554db363
Cristian Bucci <cristianivanbucci@gmail.com>
2022-10-09 09:29:52 -0300

    TG-W08: Delete and put method

--- src/index.js
+++ src/index.js
@@ -2,21 +2,18 @@
 import express from 'express';
 
 // use "require" to import JSON files
-const admins = require('./data/admins.json');
+const projectRouter = require('./resources/projects');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+// Project route
+app.use(projectRouter);
+
 app.get('/', async (req, res) => {
   res.send('Hello World!');
-});
-
-app.get('/admins', (req, res) => {
-  res.status(200).json({
-    data: admins,
-  });
 });
 
 app.listen(port, () => {
