22bba8c7b4e7ceadec06dd76165c7575167a26b1
Cristian Bucci <cristianivanbucci@gmail.com>
2022-10-10 16:32:04 -0300

    TG-W08: Add filter method to projects

--- src/resources/projects.js
+++ src/resources/projects.js
@@ -16,7 +16,6 @@ router.put('/update/:id', (req, res) => {
   const updated = projects.find((project) => project.id === parseInt(id, 10));
   const deleted = projects.filter((project) => project.id !== parseInt(id, 10));
   const empty = Object.values(reqBody).filter((value) => value === '' || value === null);
-  console.log(empty);
 
   if (!reqBody) return res.status(400).send('Bad Request');
   if (!updated) {
@@ -33,6 +32,48 @@ router.put('/update/:id', (req, res) => {
     fs.writeFile('src/data/projects.json', JSON.stringify(deleted, null, 2), (err) => (err ? res.status(400).send('Cannot delete project') : res.status(200).send('Project updated')));
   }
   return res.send('Project Updated');
+});
+
+router.get('/filter?', (req, res) => {
+  const queryParams = req.query;
+  let filter = projects;
+
+  if (Object.keys(queryParams).length > 0) {
+    const filterProjectName = queryParams.projectName;
+    const filterProjectManagerId = parseInt(queryParams.projectManagerId, 10);
+    const filterProjectStartDate = queryParams.projectStartDate;
+    filter = projects.filter((project) => Object.keys(project) === Object.keys(queryParams));
+
+    if (filterProjectName) {
+      filter = projects.filter((project) => project.projectName.includes(filterProjectName));
+    }
+    if (filterProjectManagerId) {
+      // eslint-disable-next-line max-len
+      filter = projects.filter((project) => project.projectManagerId === filterProjectManagerId);
+      if (filter.length !== 0) {
+        res.status(200).send(filter);
+      } else {
+        res.status(400).json({
+          msg: 'Invalid id',
+        });
+      }
+    } else {
+      res.status(400).json({
+        msg: 'Invalid id',
+      });
+    }
+    if (filterProjectStartDate) {
+      // eslint-disable-next-line max-len
+      filter = projects.filter((project) => project.projectStartDate.includes(filterProjectStartDate));
+    }
+    res.status(200).json({
+      project: filter,
+    });
+  } else {
+    res.status(404).json({
+      msg: 'Project not found',
+    });
+  }
 });
 
 module.exports = router;
