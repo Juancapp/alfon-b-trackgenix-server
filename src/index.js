23119a575d4199fa5290147019c3fbd20568528b
Cristian Bucci <cristianivanbucci@gmail.com>
2022-10-11 13:03:20 -0300

    TG-W08: Add data and router

--- src/index.js
+++ src/index.js
@@ -2,21 +2,16 @@
 import express from 'express';
 
 // use "require" to import JSON files
-const admins = require('./data/admins.json');
+const superAdminsRouter = require('./resources/super-admins');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use('/super-admins', superAdminsRouter);
 
 app.get('/', async (req, res) => {
   res.send('Hello World!');
-});
-
-app.get('/admins', (req, res) => {
-  res.status(200).json({
-    data: admins,
-  });
 });
 
 app.listen(port, () => {
