efa49785031b6f9c2fb6544f6e61b0040aa9252f
Ramiro <raamiroamadeo@gmail.com>
2022-10-11 13:24:36 -0300

    TG-08: created validations for method post

--- src/resources/time-sheets.js
+++ src/resources/time-sheets.js
@@ -9,7 +9,7 @@ router.get('/getAll', (req, res) => {
 });
 
 router.get('/getByid/:id', (req, res) => {
-  const userId = req.params.id;
+  const userId = parseInt(req.params.id, 10);
   const foundUser = timeSheets.find((user) => user.id === userId);
   if (foundUser) {
     res.send(foundUser);
@@ -20,18 +20,30 @@ router.get('/getByid/:id', (req, res) => {
 
 router.post('/add', (req, res) => {
   const newUser = req.body;
-  timeSheets.push(newUser);
-  fs.writeFile('src/data/time-sheets.json', JSON.stringify(timeSheets), (err) => {
-    if (err) {
-      res.send('Cant save new user');
-    } else {
-      res.send('User created');
-    }
-  });
+  const foundUser = timeSheets.find((user) => user.id === newUser.id);
+  const empty = Object.values(newUser).some((value) => value === "" );
+  const complete = Object.values (newUser).length === 6;
+ // const repeatedId =
+
+  if (!complete){
+    res.send ("Missing fields to be completed");
+  }else if (empty){
+    res.send ("There are empty fields");
+  }else if (foundUser){
+    res.send ("This id already exist");
+  }else{
+      timeSheets.push(newUser);
+      fs.writeFile('src/data/time-sheets.json', JSON.stringify(timeSheets), (err) => {
+        if (err) {
+          res.send('Cant save new user');
+        } else {
+          res.send('User created');
+        }
+      });
+  }
 });
 
 router.get('/prueba', (req, res) => {
-  console.log(req.query);
   const { typeFilter, filterValue } = req.query;
   if (typeFilter === 'proyect') {
     const usersByProyect = timeSheets.filter((users) => users.proyect === filterValue);
