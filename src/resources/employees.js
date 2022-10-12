6bd98ca6559cc86e4c15f9962c4960f3d5b85f63
Julian Viso <julian.viso.romero@gmail.com>
2022-10-12 13:00:36 -0300

    TG-08: made some fixes

--- src/resources/employees.js
+++ src/resources/employees.js
@@ -1,18 +1,18 @@
-// get y post and getById
-const express = require('express');
-const fs = require('fs');
+import express from 'express';
+import fs from 'fs';
+
 const employees = require('../data/employees.json');
 
 const router = express.Router();
 
-router.get('/list', (req, res) => {
+router.get('/', (req, res) => {
   if (employees) {
     res.status(200).send(employees);
   } else {
     res.status(404).send('There isn\'t any employee created');
   }
 });
-router.get('/getById/:id', (req, res) => {
+router.get('/:id', (req, res) => {
   const employeeId = req.params.id;
   const employee = employees.find((employ) => `${employ.id}` === employeeId);
   if (employee) {
@@ -21,7 +21,7 @@ router.get('/getById/:id', (req, res) => {
     res.status(404).send(`Can´t find user ID: ${employeeId}`);
   }
 });
-router.post('/create', (req, res) => {
+router.post('/', (req, res) => {
   const newEmployee = req.body;
   const emptyValues = Object.values(newEmployee).some((value) => value === '');
   const bodyComplete = Object.values(newEmployee).length === 8;
@@ -33,11 +33,11 @@ router.post('/create', (req, res) => {
   } else {
     employees.forEach((emp) => {
       if (`${emp.id}` === newEmployee.id) {
-        res.status(406).send('Already exist employee with this id');
+        res.status(404).send('Already exist employee with this id');
       }
     });
   }
-  if (res.status !== 400 && res.status !== 406) {
+  if (res.status !== 400 && res.status !== 404) {
     employees.push(newEmployee);
     fs.writeFile('src/data/employees.json', JSON.stringify(employees), (err) => {
       if (err) {
@@ -49,4 +49,4 @@ router.post('/create', (req, res) => {
   }
 });
 
-module.exports = router;
+export default router;
