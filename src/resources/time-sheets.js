7f98839761c4a855cebdb714d378a8039dac3bd4
Ramiro <raamiroamadeo@gmail.com>
2022-10-13 01:54:38 -0300

    TG-08: modified in base of comments

--- src/resources/time-sheets.js
+++ src/resources/time-sheets.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import fs from 'fs';
 import timeSheets from '../data/time-sheets.json';
-//const timeSheets = require('../data/time-sheets.json');
+// const timeSheets = require('../data/time-sheets.json');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-      res.status(200).json(timeSheets);
+  res.status(200).json(timeSheets);
 });
 
 router.get('/:id', (req, res) => {
@@ -15,7 +15,7 @@ router.get('/:id', (req, res) => {
   if (foundUser) {
     res.status(200).json(foundUser);
   } else {
-    res.status(404).json({message: 'User not found'});
+    res.status(404).json({ message: 'User not found' });
   }
 });
 
@@ -26,21 +26,21 @@ router.post('/', (req, res) => {
   const complete = Object.values(newUser).length === 6;
 
   if (!complete) {
-    res.status(400).json({message: 'Missing fields to be completed'});
+    res.status(400).json({ message: 'Missing fields to be completed' });
   } else if (empty) {
-    res.status(400).json({message: 'There are empty fields'});
+    res.status(400).json({ message: 'There are empty fields' });
   } else if (foundUser) {
-    res.status(400).json({message: 'This id already exist'});
+    res.status(400).json({ message: 'This id already exist' });
   } else {
     timeSheets.push(newUser);
     fs.writeFile('src/data/time-sheets.json', JSON.stringify(timeSheets), (err) => {
       if (err) {
-        res.status(400).json({message: 'Cant save new user'});
+        res.status(400).json({ message: 'Cant save new user' });
       } else {
-        res.status(200).json({message: 'User created'});
+        res.status(200).json({ message: 'User created' });
       }
     });
   }
-})
+});
 
 export default router;
