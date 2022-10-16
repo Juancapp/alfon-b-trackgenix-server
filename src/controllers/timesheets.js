9e8afb4642d6bc984cd054231bf21313bea4f967
sofihoj <sofiahojberg@gmail.com>
2022-10-15 21:46:43 -0300

    TG-10: added timesheets post method

--- src/controllers/timesheets.js
+++ src/controllers/timesheets.js
@@ -35,7 +35,30 @@ const getAllTimesheetByID = async (req, res) => {
   }
 };
 
+const createTimesheet = async (req, res) => {
+  try {
+    const timesheet = new Timesheets({
+      description: req.body.description,
+      date: req.body.date,
+      task: req.body.task,
+    });
+
+    const result = await timesheet.save();
+    return res.status(201).json({
+      message: 'Timesheet created successfully.',
+      data: result,
+      error: false,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      message: 'An error occurred',
+      error,
+    });
+  }
+};
+
 export default {
   getAllTimesheets,
   getAllTimesheetByID,
+  createTimesheet,
 };
--- src/routes/timesheets.js
+++ src/routes/timesheets.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import timesheetsControllers from '../controllers/timesheets';
+import timesheetsValidations from '../validations/timesheets';
 
 const router = express.Router();
 
 router
   .get('/', timesheetsControllers.getAllTimesheets)
-  .get('/:id', timesheetsControllers.getAllTimesheetByID);
+  .get('/:id', timesheetsControllers.getAllTimesheetByID)
+  .post('/', timesheetsValidations.validateTimeSheets, timesheetsControllers.createTimesheet);
 
 export default router;
