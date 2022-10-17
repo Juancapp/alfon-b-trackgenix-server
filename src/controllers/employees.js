fc44599110d1ba88ba17a6e1823d291a6daecc44
Ramiro <raamiroamadeo@gmail.com>
2022-10-17 00:22:27 -0300

    TG-03: created delete method

--- src/controllers/employees.js
+++ src/controllers/employees.js
@@ -1,24 +1,22 @@
-import employees from '../models/Employees'
+import employees from '../models/Employees';
 
 const deleteEmployees = async (req, res) => {
-    try {
-      const { id } = req.params;
-      const result = await employees.findByIdAndDelete(id);
-      return res.status(204).json({
-        message: `Employee with id ${id} deleted`,
-        data: result,
-        error: false,
-      });
-    } catch (error) {
-      return res.json({
-        message: 'An error occurred',
-        error: true,
-      });
-    }
-  };
-
-
+  try {
+    const { id } = req.params;
+    const result = await employees.findByIdAndDelete(id);
+    return res.status(204).json({
+      message: `Employee with id ${id} deleted`,
+      data: result,
+      error: false,
+    });
+  } catch (error) {
+    return res.json({
+      message: 'An error occurred',
+      error: true,
+    });
+  }
+};
 
 export default {
-    deleteEmployees,
+  deleteEmployees,
 };
--- src/validations/employees.js
+++ src/validations/employees.js
@@ -1,21 +1,22 @@
-import Joi from "joi";
+/* import Joi from 'joi';
 
 const validateCreation = (req, res, next) => {
-    const employeesValidation = Joi.object({
-        name: Joi.string().min(3).max(50).required(),
-        lastName: Joi.string().min(3).max(50).required(),
-        phone: Joi.string().min(8).max(15).required(),
-        email: Joi.string(),
-        password: Joi.string(),
-        dni: Joi.string(),
-    })
-    if (validation.error) {
-        return res.status(400).json({
+  const employeesValidation = Joi.object({
+    name: Joi.string().min(3).max(50).required(),
+    lastName: Joi.string().min(3).max(50).required(),
+    phone: Joi.string().min(8).max(15).required(),
+    email: Joi.string(),
+    password: Joi.string(),
+    dni: Joi.string(),
+  });
+  if (validation.error) {
+    return res.status(400).json({
 
-        })
-    }
-}
+    });
+  }
+};
 
 export default {
-    validateCreation,
-}
+  validateCreation,
+};
+ */
