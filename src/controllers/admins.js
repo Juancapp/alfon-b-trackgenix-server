6e2624ad2cb92bf220dc23154d4ee4f1d8415027
Julian Viso <julian.viso.romero@gmail.com>
2022-10-17 15:32:44 -0300

    TG-06: ADD errors validations

--- src/controllers/admins.js
+++ src/controllers/admins.js
@@ -1,5 +1,15 @@
 import Admins from '../models/Admins';
 
+const { ObjectId } = require('mongoose').Types;
+
+const isValidObjectId = (id) => {
+  if (ObjectId.isValid(id)) {
+    if ((String)(new ObjectId(id)) === id) { return true; }
+    return false;
+  }
+  return false;
+};
+
 const getAllAdmins = async (req, res) => {
   try {
     const admins = await Admins.find();
@@ -17,18 +27,28 @@ const getAllAdmins = async (req, res) => {
   }
 };
 const getAdminById = async (req, res) => {
+  if (isValidObjectId(req.param.id)) {
+    return res.status(400).json({
+      message: 'Invalid ID',
+      error: true,
+    });
+  }
   try {
-    const { id } = req.params;
-    const admin = await Admins.findById(id);
-
-    return res.status(200).json({
-      message: 'Admin found',
-      data: admin,
-      error: false,
+    const admin = await Admins.findById(req.params.id);
+    if (admin) {
+      return res.status(200).json({
+        msg: 'ID existe',
+        data: admin,
+        error: false,
+      });
+    }
+    return res.status(404).json({
+      message: 'Admin no found',
+      error: true,
     });
   } catch (error) {
-    return res.json({
-      message: 'An error ocurred',
+    return res.status(500).json({
+      message: 'ERROR',
       error,
     });
   }
@@ -44,7 +64,7 @@ const createAdmin = async (req, res) => {
       phone: req.body.phone,
     });
 
-    const result = admin.save();
+    const result = await admin.save();
     return res.status(200).json({
       message: 'Admin created',
       data: result,
--- src/validations/admins.js
+++ src/validations/admins.js
@@ -2,19 +2,21 @@ import Joi from 'joi';
 
 const validateAdmin = (req, res, next) => {
   const adminValidation = Joi.object({
-    name: Joi.string().min(3).max(50).required,
-    lastName: Joi.string().min(3).max(50).required,
-    email: Joi.string().email({ tlds: { allow: false } }).required,
-    password: Joi.string().pattern(/^[a-zA-Z0-9]{3,30}$/).required,
-    dni: Joi.string().min(6).max(12).pattern(/[0-9]/).required,
-    phone: Joi.string().min(8).max(15).pattern(/[0-9]/).required,
+    name: Joi.string().min(3).max(50).required(),
+    lastName: Joi.string().min(3).max(50).required(),
+    email: Joi.string().email({ tlds: { allow: false } }).required(),
+    password: Joi.string().pattern(/^[a-zA-Z0-9]{3,30}$/).required(),
+    dni: Joi.string().min(6).max(12).pattern(/[0-9]/)
+      .required(),
+    phone: Joi.string().min(8).max(15).pattern(/[0-9]/)
+      .required(),
   });
 
   const validation = adminValidation.validate(req.body);
 
   if (validation.error) {
-    return res.status(404).json({
-      message: `There was an error ${validation.details[0].message}`,
+    return res.status(400).json({
+      message: `There was an error ${validation.error.details[0].message}`,
       data: undefined,
       error: true,
     });
