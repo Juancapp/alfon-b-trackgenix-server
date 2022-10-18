bf4fc7eae902ffb83963a5a0d9dc51403dd223e9
Ramiro <raamiroamadeo@gmail.com>
2022-10-18 11:05:30 -0300

    TG-03: finished the methods and let ready for PR

--- src/controllers/employees.js
+++ src/controllers/employees.js
@@ -8,15 +8,21 @@ const editEmployees = async (req, res) => {
       { ...req.body },
       { new: true },
     );
-
-    return res.status(200).json({
-      message: `Employee with id ${id} edited`,
-      data: result,
-      error: false,
+    if (result) {
+      return res.status(200).json({
+        message: `Employee with id ${id} updated successfully`,
+        data: result,
+        error: false,
+      });
+    }
+    return res.status(404).json({
+      message: `Employee with id ${id} not found`,
+      data: undefined,
+      error: true,
     });
   } catch (error) {
     return res.status(500).json({
-      message: 'An error ocurred',
+      message: `Server error: ${error}`,
       error,
     });
   }
@@ -26,14 +32,23 @@ const deleteEmployees = async (req, res) => {
   try {
     const { id } = req.params;
     const result = await employees.findByIdAndDelete(id);
-    return res.status(204).json({
-      message: `Employee with id ${id} deleted`,
-      data: result,
-      error: false,
+
+    if (result) {
+      return res.status(200).json({
+        message: 'Employee deleted succesfully',
+        data: result,
+        error: false,
+      });
+    }
+    return res.status(404).json({
+      message: `Employee with id ${id} not found`,
+      data: undefined,
+      error: true,
     });
   } catch (error) {
     return res.status(500).json({
-      message: 'An error occurred',
+      message: `Server error: ${error}`,
+      data: undefined,
       error: true,
     });
   }
