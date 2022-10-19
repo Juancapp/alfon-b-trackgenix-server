42006c92efdeccf0312849b17b9737dd59f4bce8
SantiagoGonzalez97 <santiagogonzalez97@hotmail.com>
2022-10-19 13:18:58 -0300

    TG-04: Fixed comments on PR

--- src/models/Projects.js
+++ src/models/Projects.js
@@ -5,6 +5,7 @@ const { Schema } = mongoose;
 const projectSchema = new Schema({
   name: { type: String, required: true },
   startDate: { type: Date, required: true },
+  endDate: { type: Date, required: true },
   description: { type: String, required: true },
   clientName: { type: String, required: true },
   active: { type: Boolean, required: true },
