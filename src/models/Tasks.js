4c07e2537286fd415b30aaa45e9fa9eae90487f0
ProDIGGY-vinilos <cristiangerster@gmail.com>
2022-10-19 14:11:08 -0300

    TG-13: Required added to model

--- src/models/Tasks.js
+++ src/models/Tasks.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const taskSchema = new Schema({
-  description: { type: String },
+  description: { type: String, required: true },
 });
 
 export default mongoose.model('Task', taskSchema);
