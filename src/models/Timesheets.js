e7d24db6b9694e9f022af4ed6badc1be69f3ed64
Julian Viso <julian.viso.romero@gmail.com>
2022-10-22 11:42:01 -0300

    TG-18: update schema and validation timesheet

--- src/models/Timesheets.js
+++ src/models/Timesheets.js
@@ -6,6 +6,9 @@ const timesheetsSchema = new Schema({
   description: { type: String, required: true },
   date: { type: Date, required: true },
   task: { type: String, required: true },
+  hours: { type: Number, required: true },
+  employee: { type: Schema.Types.ObjectId, ref: 'Employee', required: true },
+  project: { type: Schema.Types.ObjectId, ref: 'Project', required: true },
 });
 
 export default mongoose.model('Timesheets', timesheetsSchema);
--- src/validations/timesheets.js
+++ src/validations/timesheets.js
@@ -5,13 +5,19 @@ const validateTimeSheets = (req, res, next) => {
     description: Joi.string().min(20).max(100).required(),
     date: Joi.date().required(),
     task: Joi.string().min(10).max(50).required(),
+    hours: Joi.number().integer().positive().greater(0)
+      .required(),
+    employee: Joi.string().alphanum().min(24).max(24)
+      .required(),
+    project: Joi.string().alphanum().min(24).max(24)
+      .required(),
   });
 
   const validation = timesheetsValidation.validate(req.body);
 
   if (validation.error) {
     return res.status(400).json({
-      message: `Cannot create timesheet: ${validation.details[0].message}`,
+      message: `Cannot create timesheet: ${validation.error.details[0].message}`,
       data: undefined,
       error: true,
     });
