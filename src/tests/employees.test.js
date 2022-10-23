2320ae5c80a7f583d92d083d7a404bb9b7af1424
SantiagoGonzalez97 <santiagogonzalez97@hotmail.com>
2022-10-23 20:13:31 -0300

    TG-25: Test of branch

--- src/tests/employees.test.js
+++ src/tests/employees.test.js
@@ -1,5 +1,25 @@
-describe('example', () => {
-  it('Test', () => {
-    expect(true).toEqual(true);
+import request from 'supertest';
+import app from '../app';
+import Employees from '../models/Employees';
+import employeesSeed from '../seeds/employees';
+
+beforeAll(async () => {
+  await Employees.collection.insertMany(employeesSeed);
+});
+
+let employeeId;
+
+describe('DELETE /employees', () => {
+  test('Should delete an employee', async () => {
+    const response = await request(app).delete(`/employees/${employeeId}`).send();
+    // eslint-disable-next-line no-underscore-dangle
+    employeeId = response.body.data._id;
+    expect(response.status).toBe(200);
   });
 });
+
+// describe('example', () => {
+//   it('Test', () => {
+//     expect(true).toEqual(true);
+//   });
+// });
