42b8d31b6f80e4cd49046ff644310d4a57286b6b
SantiagoGonzalez97 <santiagogonzalez97@hotmail.com>
2022-10-24 20:45:50 -0300

    TG-25: Test of update and delete working succsesfully

--- src/tests/employees.test.js
+++ src/tests/employees.test.js
@@ -1,5 +1,4 @@
 import request from 'supertest';
-import mongoose from 'mongoose';
 import app from '../app';
 import Employees from '../models/Employees';
 import employeesSeeds from '../seeds/employees';
@@ -8,9 +7,30 @@ beforeAll(async () => {
   await Employees.collection.insertMany(employeesSeeds);
 });
 
+const invalidId = '6354039c6d5cab252b86b588Nvalid';
+const validId = '6354039c6d5cab252b86b580';
+const notFoundId = '6354039c6d5cab252b86b588';
+
 const mockedEmployee = {
   name: 'test',
-  last_name: 'test',
+  lastName: 'test',
+  phone: '123456789',
+  email: 'test@test.com',
+  password: 'abcd1234',
+  dni: '1234567',
+};
+
+const incompleteEmployee = {
+  name: 'test',
+  lastName: 'test',
+  phone: '123456789',
+  email: 'test@test.com',
+  password: 'abcd1234',
+};
+
+const badEmployee = {
+  name: 'te',
+  lastName: 'test',
   phone: '123456789',
   email: 'test@test.com',
   password: 'abcd1234',
@@ -24,26 +44,66 @@ describe('DELETE /employees', () => {
     const response = await request(app).delete(`/employees/${employeeId}`).send();
 
     expect(response.status).toBe(200);
+    expect(response.body.error).toBeFalsy();
+    expect(response.body.message).toBe('Employee deleted succesfully');
+    expect(response.body.data).toBeDefined();
+  });
+  test('Should return bad request', async () => {
+    const response = await request(app).delete(`/employees/${notFoundId}`).send();
+
+    expect(response.status).toBe(404);
+    expect(response.body.error).toBe(true);
+    expect(response.body.message).toBe(`Employee with id ${notFoundId} not found`);
+    expect(response.body.data).toBeUndefined();
+  });
+  test('Should not be valid', async () => {
+    const response = await request(app).put(`/employees/${invalidId}`).send(mockedEmployee);
+
+    expect(response.status).toBe(400);
+    expect(response.body.error).toBe(true);
+    expect(response.body.message).toBe(`Employee id ${invalidId} not valid`);
+    expect(response.body.data).toBeUndefined();
   });
 });
 
 describe('PUT /employees', () => {
   test('Should update employee', async () => {
-    const employeeId = mongoose.Types.ObjectId('6354039c6d5cab252b86b580');
-    const response = await request(app).put(`/employees/${employeeId}`).send(mockedEmployee);
+    const response = await request(app).put(`/employees/${validId}`).send(mockedEmployee);
 
     expect(response.status).toBe(200);
     expect(response.body.error).toBeFalsy();
-    expect(response.body.message).toBe(`Employee with id ${employeeId} updated successfully`);
+    expect(response.body.message).toBe(`Employee with id ${validId} updated successfully`);
     expect(response.body.data).toBeDefined();
   });
+  test('Should not update an admin because all fields are required', async () => {
+    const response = await request(app).put(`/employees/${validId}`).send(incompleteEmployee);
+
+    expect(response.status).toBe(400);
+    expect(response.body.error).toBe(true);
+    expect(response.body.data).toBeUndefined();
+  });
+  test('Should not update an admin because the body data is not valid', async () => {
+    const response = await request(app).put(`/employees/${validId}`).send(badEmployee);
+
+    expect(response.status).toBe(400);
+    expect(response.body.error).toBe(true);
+    expect(response.body.data).toBeUndefined();
+  });
   test('Should return bad request', async () => {
-    const employeeId = mongoose.Types.ObjectId('6354039c6d5cab252b86b588');
-    const response = await request(app).put(`/employees/${employeeId}`).send(mockedEmployee);
+    const response = await request(app).put(`/employees/${notFoundId}`).send(mockedEmployee);
 
     expect(response.status).toBe(404);
     expect(response.body.error).toBe(true);
-    expect(response.body.message).toBe(`Employee with id ${employeeId} not found`);
+    expect(response.body.message).toBe(`Employee with id ${notFoundId} not found`);
+    expect(response.body.data).toBeUndefined();
+  });
+  test('Should not be valid', async () => {
+    const response = await request(app).put(`/employees/${invalidId}`).send(mockedEmployee);
+
+    expect(response.status).toBe(400);
+    expect(response.body.error).toBe(true);
+    expect(response.body.message).toBe(`Employee id ${invalidId} not valid`);
+    expect(response.body.data).toBeUndefined();
   });
 });
 
