1fb037535a805b120f7efeb15e8dea348fd3d9ec
Amilcar Hernandez <amilcarjh24@gmail.com>
2022-11-06 13:21:36 -0300

    fixed validations message & routes

--- src/validations/admins.js
+++ src/validations/admins.js
@@ -1,6 +1,6 @@
 import Joi from 'joi';
 
-const validateAdmin = (req, res, next) => {
+const validateNewUser = (req, res, next) => {
   const adminValidation = Joi.object({
     name: Joi.string().min(3).max(50).required(),
     lastName: Joi.string().min(3).max(50).required(),
@@ -14,7 +14,7 @@ const validateAdmin = (req, res, next) => {
 
   if (validation.error) {
     return res.status(400).json({
-      message: `Cannot create admin: ${validation.error.details[0].message}`,
+      message: `Cannot create user: ${validation.error.details[0].message}`,
       data: undefined,
       error: true,
     });
@@ -22,4 +22,29 @@ const validateAdmin = (req, res, next) => {
   return next();
 };
 
-export default { validateAdmin };
+const validateUpdateUser = (req, res, next) => {
+  const adminValidation = Joi.object({
+    name: Joi.string().min(3).max(50).required(),
+    lastName: Joi.string().min(3).max(50).required(),
+    phone: Joi.number().min(10000000).max(999999999999999).required(),
+    email: Joi.string().email({ tlds: { allow: false } }).required(),
+    password: Joi.string().pattern(/^[a-zA-Z0-9]{8,50}$/).required(),
+    dni: Joi.number().min(100000).max(999999999999).required(),
+  });
+
+  const validation = adminValidation.validate(req.body);
+
+  if (validation.error) {
+    return res.status(400).json({
+      message: `Cannot edit user: ${validation.error.details[0].message}`,
+      data: undefined,
+      error: true,
+    });
+  }
+  return next();
+};
+
+export default {
+  validateNewUser,
+  validateUpdateUser,
+};
